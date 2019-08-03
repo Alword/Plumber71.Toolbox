ae7e8bebfc5e53533be1ab9a580a6f191ac3fc8f
Alword <anzer987@yandex.ru>
2019-08-03 14:31:41 +0300

    folders

--- Plumber71.TestConsole/Program.cs
+++ Plumber71.TestConsole/Program.cs
@@ -6,7 +6,7 @@ namespace Plumber71.TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("}|{OPA");
         }
     }
 }
