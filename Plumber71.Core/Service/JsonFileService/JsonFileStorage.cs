b61a7cbb41a611e9b2c3fa1d4eb78a52b584a347
Alword <anzer987@yandex.ru>
2019-08-05 21:00:44 +0300

    fix name resolve

--- Plumber71.Core/Service/JsonFileService/JsonFileStorage.cs
+++ Plumber71.Core/Service/JsonFileService/JsonFileStorage.cs
@@ -7,22 +7,26 @@ namespace Plumber71.Core.Service.JsonFileService
     public class JsonFileStorage
     {
         private static readonly string DefaultDirectory = $"{Environment.CurrentDirectory}\\Resource\\Temp";
-        public static void Save<T>(T chacheObject, string fileName = nameof(T) + ".json") where T : new()
+        public static void Save<T>(T chacheObject, string fileName = "") where T : new()
         {
+            string path = GetObjectPath<T>(fileName);
             string chacheText = JsonConvert.SerializeObject(chacheObject);
-            string path = Path.Combine(DefaultDirectory, fileName);
-            Directory.CreateDirectory(DefaultDirectory); //Access to the path 'D:\YandexDisk\Workspaces\Plumber71.Toolbox\Plumber71.TestConsole\bin\Debug\netcoreapp2.2\Temp\chacheObject' is denied.
+            Directory.CreateDirectory(DefaultDirectory); 
             File.WriteAllText(path, chacheText);
         }
-        public static T Load<T>(string fileName = nameof(T) + ".json") where T : new()
+        public static T Load<T>(string fileName = null) where T : new()
         {
-            string path = Path.Combine(DefaultDirectory, fileName);
-
+            string path = GetObjectPath<T>(fileName);
             if (!File.Exists(path)) return default;
-
             string chacheText = File.ReadAllText(path);
             T chacheObject = JsonConvert.DeserializeObject<T>(chacheText);
             return chacheObject;
+        }
+
+        private static string GetObjectPath<T>(string fileName)
+        {
+            fileName = fileName ?? $"{typeof(T).Name}";
+            return Path.Combine(DefaultDirectory, fileName);
         }
     }
 }
