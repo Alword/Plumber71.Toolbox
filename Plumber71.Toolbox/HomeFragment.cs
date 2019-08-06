bd6810b82d5a9aca7775d6829668da6c6ee4c3b4
Alword <anzer987@yandex.ru>
2019-08-06 20:16:11 +0300

    feature button frame

---
+++ Plumber71.Toolbox/HomeFragment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Plumber71.Toolbox
+{
+    public class HomeFragment : Android.Support.V4.App.Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var inflatedView = inflater.Inflate(Resource.Layout.HomeFragment, container, false);
+
+            Button excelLoad = inflatedView.FindViewById<Button>(Resource.Id.excelLoad);
+            excelLoad.Click += ExcelLoad_Click;
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            return inflater.Inflate(Resource.Layout.HomeFragment, container, false);
+        }
+
+        private void ExcelLoad_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- Plumber71.Toolbox/MainActivity.cs
+++ Plumber71.Toolbox/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
+using Android.Support.V4.App;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
@@ -11,7 +12,6 @@ namespace Plumber71.Toolbox
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
     {
-        TextView textMessage;
         FrameLayout fragmentContainer;
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -24,28 +24,33 @@ namespace Plumber71.Toolbox
 
             BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
             navigation.SetOnNavigationItemSelectedListener(this);
+            SupportFragmentManager.BeginTransaction().Replace(Resource.Id.fragmentContainer, new HomeFragment()).Commit();
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
         public bool OnNavigationItemSelected(IMenuItem item)
         {
+            Android.Support.V4.App.Fragment selectedFragment = null;
             switch (item.ItemId)
             {
                 case Resource.Id.navigation_home:
-                    textMessage.SetText(Resource.String.title_home);
-                    return true;
+                    selectedFragment = new HomeFragment();
+                    break;
                 case Resource.Id.navigation_dashboard:
-                    textMessage.SetText(Resource.String.title_dashboard);
-                    return true;
+                    selectedFragment = new TableFragment();
+                    break;
                 case Resource.Id.navigation_notifications:
-                    textMessage.SetText(Resource.String.title_notifications);
-                    return true;
+                    return false;
             }
-            return false;
+
+            SupportFragmentManager.BeginTransaction().Replace(Resource.Id.fragmentContainer, selectedFragment).Commit();
+
+            return true;
         }
     }
 }
